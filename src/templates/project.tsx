24d8bdf04e05a5e0b0806a3a4ad5829fdbd4e193
Greegko <greegko+dev@gmail.com>
2020-03-07 21:16:30 +0100

    fix project context

--- src/templates/project.tsx
+++ src/templates/project.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Layout } from '../components/layout';
 
-const ProjectTemplate = ({ location, content, isArtisticWork }) => (
+const ProjectTemplate = ({ location, pageContext: { content, isArtisticWork } }) => (
   <Layout location={location}>
     {documentToReactComponents(content)}
   </Layout>
