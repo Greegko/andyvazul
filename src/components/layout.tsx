e9e96c4079f5f3e8441c7367b421179f923f173c
Greegko <greegko+dev@gmail.com>
2020-03-22 18:12:15 +0100

    menu

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,92 +15,85 @@ interface LayoutProperties {
 
 const TitleHeight = 29;
 
-let LastActivePos = 0;
-
 const Padding = 10;
 
 import './layout.css';
 export const Layout = ({ children, location, submenu, title, description }: LayoutProperties) => {
-  const [mainPath, subPath] = location.pathname.split("/").splice(1);
-  const [activeSubmenu, setActiveSubmenu] = React.useState(subPath);
-  const [activeMenuPos, setActiveMenuPos] = React.useState<number>(LastActivePos);
-  const [submenuItems, setSubmenuItems] = React.useState(submenu || []);
-  const [contentHeight, setContentHeight] = React.useState(null);
+  const [mainMenuPath, submenuPath] = location.pathname.split("/").splice(1);
 
-  const setActiveMenuPosSave = (value: number) => {
-    LastActivePos = value;
-    setActiveMenuPos(value);
-  }
+  const [menuItems] = React.useState<MenuItem[]>(getMenuItems());
+  const [activeSubmenu, setActiveSubmenu] = React.useState<string>(() => submenuPath);
+  const [activeMenuPos, setActiveMenuPos] = React.useState<number>(0);
+  const [submenuItems, setSubmenuItems] = React.useState<MenuItem[]>(() => submenu || []);
+  const [contentHeight, setContentHeight] = React.useState<number>(0);
+  const [contentLinks, setContentLinks] = React.useState<HTMLElement[]>([]);
+  const [submenuLinks, setSubmenuLinks] = React.useState<HTMLLinkElement[]>([]);
 
-  const addSubmenuItem = (item: MenuItem) => {
-    setSubmenuItems(items => [...items, { ...item, order: items.length }]);
-  }
+  const addSubmenuItem = (item: MenuItem) => setSubmenuItems(items => [...items, { ...item, order: items.length }]);
 
-  const menu = getMenuItems();
-  const activeMenu = menu.find(x => x.path === mainPath)?.path;
+  const submenuRef = React.useCallback(node => {
+    if (!node) return;
 
-  const submenuRef = React.createRef<HTMLDivElement>();
-  const contentRef = React.createRef<HTMLDivElement>();
+    const links = [...node.querySelectorAll('ul li a')] as HTMLLinkElement[];
+    setSubmenuLinks(links);
+  }, [children, submenuItems]);
 
-  const getMenuNodes = () => submenuRef.current.querySelectorAll<HTMLAnchorElement>('ul li a');
-  const getContentLinkNodes = () => contentRef.current.querySelectorAll('.submenu-link');
+  const contentRef = React.useCallback(node => {
+    if (!node) return;
+
+    const links = [...node.querySelectorAll('.submenu-link')] as HTMLElement[];
+    setContentLinks(links);
+  }, [children]);
+
   const y = x => {
     const pos = x.getClientRects()[0];
     return pos ? Math.floor(pos.y) : null;
   }
 
   React.useEffect(() => {
-    if (contentRef.current && submenuRef.current) {
-      const submenu = getMenuNodes();
-      const lastSubmenu = submenu[submenu.length - 1];
+    if (activeMenuPos === 0) return;
+    if (contentLinks.length === 0) return;
+    if (submenuLinks.length === 0) return;
 
-      const links = getContentLinkNodes();
-      const lastLink = links[links.length - 1];
-      setContentHeight(document.body.clientHeight - y(lastSubmenu) + y(lastLink) - activeMenuPos - Padding);
-    }
-
-  }, [submenuItems, activeMenuPos]);
-
-  const scrollToMenuContent = (targetContent, menuOrder) => {
-    window.scrollTo(0, y(targetContent) - activeMenuPos - Padding - 19 * menuOrder);
-  }
+    const lastSubmenu = submenuLinks[submenuLinks.length - 1];
+    const lastLink = contentLinks[contentLinks.length - 1];
+    const pos = document.body.clientHeight - y(lastSubmenu) + lastLink.offsetTop - activeMenuPos - Padding;
+    setContentHeight(pos);
+  }, [activeMenuPos, contentLinks, submenuLinks]);
 
   React.useEffect(() => {
-    if (!submenuRef.current) return;
+    if (activeMenuPos === 0) return;
+    if (contentHeight === 0) return;
+    if (contentLinks.length === 0) return;
+    if (submenuLinks.length === 0) return;
+    if (!submenuPath) return;
 
-    if (subPath) {
-      const submenu = getMenuNodes();
-      const menuIndex = Array.from(submenu).findIndex(x => x.href.endsWith("/" + subPath));
-      const contentLink = contentRef.current.querySelector('#submenu-' + subPath);
-      scrollToMenuContent(contentLink, menuIndex);
-    } else {
-      scrollLocationCheck();
-    }
-  }, [location.pathname]);
-
-  const scrollLocationCheck = () => {
-    if (!submenuRef.current) return;
-
-    const submenus = getMenuNodes();
-    const contents = getContentLinkNodes();
-
-    const menuIndex = Math.max(0, Array.from(contents).filter((x, index) => y(x) <= y(submenus[index])).length - 1);
-    const menuHref = submenus[menuIndex].attributes.getNamedItem('href').nodeValue;
-    const submenuPart = menuHref.split('/')[2];
-
-    if (submenuPart !== activeSubmenu) {
-      setActiveSubmenu(submenuPart);
-    }
-
-    window.history.pushState({}, null, menuHref);
-  };
+    const menuIndex = submenuLinks.findIndex(x => x.href.endsWith("/" + submenuPath));
+    const contentLink = contentLinks.find(x => x.id === 'submenu-' + submenuPath);
+    const pos = contentLink.offsetTop - activeMenuPos - Padding - 19 * menuIndex;
+    window.scrollTo(0, pos);
+  }, [location.pathname, submenuPath, submenuLinks, contentLinks, contentHeight]);
 
   React.useEffect(() => {
-    if (submenuRef.current) {
-      window.addEventListener('scroll', scrollLocationCheck, true);
-      return () => window.removeEventListener('scroll', scrollLocationCheck, true);
-    }
-  });
+    if (contentHeight === 0) return;
+    if (contentLinks.length === 0) return;
+    if (submenuLinks.length === 0) return;
+
+    const scrollLocationCheck = () => {
+      const menuIndex = Math.max(0, contentLinks.filter((x, index) => y(x) <= y(submenuLinks[index])).length - 1);
+      const menuHref = submenuLinks[menuIndex].attributes.getNamedItem('href').nodeValue;
+      const submenuPart = menuHref.split('/')[2];
+
+      if (submenuPart !== activeSubmenu) {
+        setActiveSubmenu(submenuPart);
+      }
+
+      window.history.pushState({}, null, menuHref);
+    };
+
+    window.addEventListener('scroll', scrollLocationCheck, true);
+    return () => window.removeEventListener('scroll', scrollLocationCheck, true);
+  }, [submenuLinks, contentLinks, contentHeight, activeSubmenu]);
 
   return (
     <div className="layout">
@@ -112,10 +105,10 @@ export const Layout = ({ children, location, submenu, title, description }: Layo
         <div className="sidebar-content">
           <div className="title"><Link to="/">andyvazul</Link></div>
           <div className="menus">
-            <Menu items={menu} activeItem={activeMenu} base="" setActiveElementPosition={setActiveMenuPosSave} />
+            <Menu items={menuItems} activeItem={mainMenuPath} base="" setActiveElementPosition={setActiveMenuPos} />
             {submenuItems.length > 0 &&
               <div ref={submenuRef} className="submenu" style={{ paddingTop: activeMenuPos - TitleHeight }}>
-                <Menu items={submenuItems} activeItem={activeSubmenu} base={"/" + activeMenu} />
+                <Menu items={submenuItems} activeItem={activeSubmenu} base={"/" + mainMenuPath} />
               </div>
             }
           </div>
@@ -123,7 +116,7 @@ export const Layout = ({ children, location, submenu, title, description }: Layo
       </div>
       <div ref={contentRef} className="content" style={{ paddingTop: activeMenuPos }}>
         <SubmenuContext.Provider value={{ addSubmenuItem }}>
-          <div className="content-body" style={{ minHeight: contentHeight }}>
+          <div className="content-body" style={{ height: contentHeight }}>
             {children}
           </div>
         </SubmenuContext.Provider>
