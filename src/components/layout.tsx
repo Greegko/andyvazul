ff96032d014347c70e317c4a358e42114d79bb3a
Greegko <greegko+dev@gmail.com>
2020-03-27 18:34:27 +0100

    update page size

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,36 +14,78 @@ interface LayoutProperties {
 }
 
 const TitleHeight = 29;
-
 const Padding = 10;
 
 import './layout.css';
 export const Layout = ({ children, location, submenu, title, description }: LayoutProperties) => {
-  const [mainMenuPath, submenuPath] = location.pathname.split("/").splice(1);
+  const [mainMenuPath, submenuPath, projectPath] = location.pathname.split("/").splice(1);
 
-  const [menuItems] = React.useState<MenuItem[]>(getMenuItems());
   const [activeSubmenu, setActiveSubmenu] = React.useState<string>(() => submenuPath);
   const [activeMenuPos, setActiveMenuPos] = React.useState<number>(0);
+  const [submenuMenuPos, setSubmenuMenuPos] = React.useState<number>(0);
   const [submenuItems, setSubmenuItems] = React.useState<MenuItem[]>(() => submenu || []);
+
+  const addSubmenuItem = (item: MenuItem) => setSubmenuItems(items => [...items, { ...item, order: items.length }]);
+
+  const displayCoreProperties: LayoutDisplayCore = {
+    title,
+    description,
+    submenuItems,
+    mainMenuPath,
+    contentPadding: submenuMenuPos || activeMenuPos,
+    activeSubmenu,
+    menuPadding: activeMenuPos,
+    children,
+    setActiveMenuPos,
+    setSubmenuMenuPos,
+  }
+
+  const layoutContent = projectPath ?
+    <LayoutDisplay {...displayCoreProperties} /> :
+    <LayoutSubmenuScroll
+      {...displayCoreProperties}
+      activeMenuPos={activeMenuPos}
+      setActiveSubmenu={setActiveSubmenu}
+      submenuPath={submenuPath}
+    />
+
+  return (
+    <SubmenuContext.Provider value={{ addSubmenuItem }}>
+      {layoutContent}
+    </SubmenuContext.Provider>
+  );
+};
+
+interface LayoutSubmenuScrollProperties {
+  activeMenuPos;
+  setActiveSubmenu;
+  submenuPath;
+}
+
+const LayoutSubmenuScroll = (props: LayoutSubmenuScrollProperties & LayoutDisplayCore) => {
   const [contentHeight, setContentHeight] = React.useState<number>(0);
   const [contentLinks, setContentLinks] = React.useState<HTMLElement[]>([]);
   const [submenuLinks, setSubmenuLinks] = React.useState<HTMLLinkElement[]>([]);
-
-  const addSubmenuItem = (item: MenuItem) => setSubmenuItems(items => [...items, { ...item, order: items.length }]);
 
   const submenuRef = React.useCallback(node => {
     if (!node) return;
 
     const links = [...node.querySelectorAll('ul li a')] as HTMLLinkElement[];
     setSubmenuLinks(links);
-  }, [children, submenuItems]);
+
+    if (!props.activeSubmenu) {
+      const menuHref = links[0].attributes.getNamedItem('href').nodeValue;
+      const submenuPart = menuHref.split('/')[2];
+      props.setActiveSubmenu(submenuPart);
+    }
+  }, [props.children, props.submenuItems]);
 
   const contentRef = React.useCallback(node => {
     if (!node) return;
 
     const links = [...node.querySelectorAll('.submenu-link')] as HTMLElement[];
     setContentLinks(links);
-  }, [children]);
+  }, [props.children]);
 
   const y = x => {
     const pos = x.getClientRects()[0];
@@ -51,28 +93,28 @@ export const Layout = ({ children, location, submenu, title, description }: Layo
   }
 
   React.useEffect(() => {
-    if (activeMenuPos === 0) return;
+    if (props.activeMenuPos === 0) return;
     if (contentLinks.length === 0) return;
     if (submenuLinks.length === 0) return;
 
     const lastSubmenu = submenuLinks[submenuLinks.length - 1];
     const lastLink = contentLinks[contentLinks.length - 1];
-    const pos = document.body.clientHeight - y(lastSubmenu) + lastLink.offsetTop - activeMenuPos - Padding;
+    const pos = document.body.clientHeight - y(lastSubmenu) + lastLink.offsetTop - props.activeMenuPos - Padding;
     setContentHeight(pos);
-  }, [activeMenuPos, contentLinks, submenuLinks]);
+  }, [props.activeMenuPos, contentLinks, submenuLinks]);
 
   React.useEffect(() => {
-    if (activeMenuPos === 0) return;
+    if (props.activeMenuPos === 0) return;
+    if (!props.submenuPath) return;
     if (contentHeight === 0) return;
     if (contentLinks.length === 0) return;
     if (submenuLinks.length === 0) return;
-    if (!submenuPath) return;
 
-    const menuIndex = submenuLinks.findIndex(x => x.href.endsWith("/" + submenuPath));
-    const contentLink = contentLinks.find(x => x.id === 'submenu-' + submenuPath);
-    const pos = contentLink.offsetTop - activeMenuPos - Padding - 19 * menuIndex;
+    const menuIndex = submenuLinks.findIndex(x => x.href.endsWith("/" + props.submenuPath));
+    const contentLink = contentLinks.find(x => x.id === 'submenu-' + props.submenuPath);
+    const pos = contentLink ? (contentLink.offsetTop - props.activeMenuPos - Padding - 19 * menuIndex) : 0;
     window.scrollTo(0, pos);
-  }, [location.pathname, submenuPath, submenuLinks, contentLinks, contentHeight]);
+  }, [props.submenuPath, submenuLinks, contentLinks, contentHeight]);
 
   React.useEffect(() => {
     if (contentHeight === 0) return;
@@ -84,8 +126,8 @@ export const Layout = ({ children, location, submenu, title, description }: Layo
       const menuHref = submenuLinks[menuIndex].attributes.getNamedItem('href').nodeValue;
       const submenuPart = menuHref.split('/')[2];
 
-      if (submenuPart !== activeSubmenu) {
-        setActiveSubmenu(submenuPart);
+      if (submenuPart !== props.activeSubmenu) {
+        props.setActiveSubmenu(submenuPart);
       }
 
       window.history.pushState({}, null, menuHref);
@@ -93,34 +135,62 @@ export const Layout = ({ children, location, submenu, title, description }: Layo
 
     window.addEventListener('scroll', scrollLocationCheck, true);
     return () => window.removeEventListener('scroll', scrollLocationCheck, true);
-  }, [submenuLinks, contentLinks, contentHeight, activeSubmenu]);
+  }, [submenuLinks, contentLinks, contentHeight, props.activeSubmenu]);
+
+  return <LayoutDisplay
+    {...props}
+    submenuRef={submenuRef}
+    contentRef={contentRef}
+    contentHeight={contentHeight}
+  />;
+}
+
+interface LayoutDisplayCore {
+  title;
+  description;
+  submenuItems;
+  mainMenuPath;
+  setActiveMenuPos;
+  contentPadding;
+  activeSubmenu;
+  menuPadding;
+  setSubmenuMenuPos;
+  children;
+}
+
+interface LayoutDisplayScroll {
+  submenuRef?;
+  contentRef?;
+  contentHeight?;
+}
+
+const LayoutDisplay = (props: LayoutDisplayCore & LayoutDisplayScroll) => {
+  const [menuItems] = React.useState<MenuItem[]>(getMenuItems());
 
   return (
     <div className="layout">
       <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
+        <title>{props.title}</title>
+        <meta name="description" content={props.description} />
       </Helmet>
-      <div className={"sidebar" + (submenuItems.length > 0 ? " sidebar--submenu" : "")}>
+      <div className={"sidebar" + (props.submenuItems.length > 0 ? " sidebar--submenu" : "")}>
         <div className="sidebar-content">
           <div className="title"><Link to="/">andyvazul</Link></div>
           <div className="menus">
-            <Menu items={menuItems} activeItem={mainMenuPath} base="" setActiveElementPosition={setActiveMenuPos} />
-            {submenuItems.length > 0 &&
-              <div ref={submenuRef} className="submenu" style={{ paddingTop: activeMenuPos - TitleHeight }}>
-                <Menu items={submenuItems} activeItem={activeSubmenu} base={"/" + mainMenuPath} />
+            <Menu items={menuItems} activeItem={props.mainMenuPath} base="" setActiveElementPosition={props.setActiveMenuPos} />
+            {props.submenuItems.length > 0 &&
+              <div ref={props.submenuRef} className="submenu" style={{ paddingTop: props.menuPadding - TitleHeight }}>
+                <Menu items={props.submenuItems} activeItem={props.activeSubmenu} base={"/" + props.mainMenuPath} setActiveElementPosition={props.setSubmenuMenuPos} />
               </div>
             }
           </div>
         </div>
       </div>
-      <div ref={contentRef} className="content" style={{ paddingTop: activeMenuPos }}>
-        <SubmenuContext.Provider value={{ addSubmenuItem }}>
-          <div className="content-body" style={{ height: contentHeight }}>
-            {children}
-          </div>
-        </SubmenuContext.Provider>
+      <div ref={props.contentRef} className="content" style={{ paddingTop: props.contentPadding }}>
+        <div className="content-body" style={{ height: props.contentHeight }}>
+          {props.children}
+        </div>
       </div>
     </div>
   );
-};
+}
