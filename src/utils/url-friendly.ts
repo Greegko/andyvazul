ae43b1634bef1b317e787fc70c049c51d3bc8844
Greegko <greegko+dev@gmail.com>
2020-03-29 20:14:30 +0200

    urlFriendly fix

--- src/utils/url-friendly.ts
+++ src/utils/url-friendly.ts
@@ -1,1 +1,7 @@
-export const urlFriendly = (path: string | number) => path.toString().toLowerCase().replace(/[^\w]/g, '-');
+export const urlFriendly = (path: string) =>
+  path
+    .toString()
+    .toLowerCase()
+    .replace(/[^\w]/g, '-')
+    .replace(/(\w)\W+$/, "\$1")
+    .replace(/^\W+(\w)/, "\$1");
