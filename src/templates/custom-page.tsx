bd4d1f237be5f9e7d8e65eea6d7073dee8e71b9d
Greegko <greegko+dev@gmail.com>
2020-03-30 00:46:00 +0200

    remove empty new line feeds

--- src/templates/custom-page.tsx
+++ src/templates/custom-page.tsx
@@ -6,9 +6,18 @@ import './custom-page.scss';
 export default function CustomPage({ location, pageContext: { content, title, description } }) {
   const displayAsBlock = location.pathname === "/";
 
+  content.children = content.children.reduce((acc, value) => {
+    if (acc[1] && isNewLine(value)) return acc;
+
+    return [[...acc[0], value], isSubmenuItem(value)];
+  }, [[], false])[0];
+
   return (
     <Layout location={location} title={title} description={description} displayAsBlock={displayAsBlock}>
       {render(content)}
     </Layout>
   );
 };
+
+const isSubmenuItem = node => node.type === 'element' && node.children.length === 1 && node.children[0].tagName === 'submenu-item';
+const isNewLine = node => node.type === "text" && node.value.match(/^\s$/) !== null;
