2ec6ea26ecf9f374e3ef5819cd070ce836cc03bc
Greegko <greegko+dev@gmail.com>
2020-04-07 23:04:22 +0200

    add contentEditable flag

--- src/components/stern.tsx
+++ src/components/stern.tsx
@@ -3,5 +3,5 @@ import * as React from 'react';
 import './stern.scss';
 export const Stern = ({ children }) => {
   const content = Array.isArray(children) ? children[0] : children;
-  return <div className="stern">{content.toLocaleUpperCase()}</div>
+  return <div className="stern" contentEditable>{content.toLocaleUpperCase()}</div>
 };
