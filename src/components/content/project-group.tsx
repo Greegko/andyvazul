4f4da983948e29c951653df06dc6496695afa587
Greegko <greegko+dev@gmail.com>
2020-04-07 23:13:21 +0200

    update project group style

--- src/components/content/project-group.tsx
+++ src/components/content/project-group.tsx
@@ -6,9 +6,7 @@ export const ProjectGroup = ({ children }) => {
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: 1 }}>{items[0]}</div>
-      <div style={{ flex: 0, flexBasis: "100px" }}>{items[1]}</div>
-      <div style={{ flex: 0, flexBasis: "100px" }}>{items[2]}</div>
-      <div style={{ flex: 0, flexBasis: "100px" }}>{items[3]}</div>
+      <div style={{ flex: 0 }}>{items[1]}</div>
     </div>
   );
 };
